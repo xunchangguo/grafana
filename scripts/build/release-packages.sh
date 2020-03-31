04cc3857baa350c6316cce2f1679d95c8e02d196
guoxunchang <guoxunchang@chalco-steering.com>
2020-03-31 09:55:12 +0800

    release

--- scripts/build/release-packages.sh
+++ scripts/build/release-packages.sh
@@ -43,10 +43,11 @@ echo $'\nPublishing packages'
 yarn packages:${SCRIPT}
 
 # When releasing stable(latest) version of packages we are updating previously published next tag(beta) to be the same version as latest
-#if [ $RELEASE_CHANNEL == "latest" ]; then
-#  for i in "${PACKAGES[@]}"
-#  do
-#    :
+if [ $RELEASE_CHANNEL == "latest" ]; then
+  for i in "${PACKAGES[@]}"
+  do
+    :
+	echo "$i"@"$PACKAGE_VERSION"
 #    npm dist-tag add "$i"@"$PACKAGE_VERSION" next
-#  done
-#fi
+  done
+fi
